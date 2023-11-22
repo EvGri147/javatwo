ca6d617eb05694d074c9d09a80768c3445fffc85
EvGri <evgeniigribanov@mail.ru>
2023-11-22 20:46:22 +0600

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 14500; // стоимость билета
-        int y = 20; // количество рублей для одной бонусной мили
+        int price = 14500; // стоимость билета
+        int amount = 20; // количество рублей для одной бонусной мили
 
-        System.out.println(x / y);
+        System.out.println(price / amount);
     }
 }
